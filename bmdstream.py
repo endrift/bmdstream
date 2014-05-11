2b01814191b37df119847c57c4c19029acabca90
Jeffrey Pfau <jeffrey@endrift.com>
2014-05-10 17:03:00 -0700

    Interaudio sink/src for non-blocking buffer in Python version

--- bmdstream.py
+++ bmdstream.py
@@ -11,20 +11,17 @@ Gst.init(None)
 class AudioResampler(Gst.Bin):
 	def __init__(self):
 		super(AudioResampler, self).__init__()
-		convert = Gst.ElementFactory.make('audioconvert', None)
 		resample = Gst.ElementFactory.make('audioresample', None)
 		capsfilter = Gst.ElementFactory.make('capsfilter', None)
 
 		capsfilter.set_property('caps', Gst.caps_from_string('audio/x-raw, rate=44100'))
 
-		self.add(convert)
 		self.add(resample)
 		self.add(capsfilter)
 
-		convert.link(resample)
 		resample.link(capsfilter)
 
-		self.add_pad(Gst.GhostPad.new('sink', convert.get_static_pad('sink')))
+		self.add_pad(Gst.GhostPad.new('sink', resample.get_static_pad('sink')))
 		self.add_pad(Gst.GhostPad.new('src', capsfilter.get_static_pad('src')))
 
 class LameBin(Gst.Bin):
@@ -80,6 +77,12 @@ src.set_property('mode', 18)
 aqueue = Gst.ElementFactory.make('queue', None)
 vqueue = Gst.ElementFactory.make('queue', None)
 
+intersink = Gst.ElementFactory.make('interaudiosink')
+intersink.set_property('channel', 'ach')
+intersrc = Gst.ElementFactory.make('interaudiosrc')
+intersrc.set_property('channel', 'ach')
+
+aconv = Gst.ElementFactory.make('audioconvert')
 ars = AudioResampler()
 aenc = LameBin()
 venc = X264Bin()
@@ -93,6 +96,9 @@ filesink.set_property('location', 'test.flv')
 pipeline.add(src)
 pipeline.add(aqueue)
 pipeline.add(vqueue)
+pipeline.add(intersink)
+pipeline.add(intersrc)
+pipeline.add(aconv)
 pipeline.add(ars)
 pipeline.add(aenc)
 pipeline.add(venc)
@@ -102,7 +108,9 @@ pipeline.add(filesink)
 src.link(aqueue)
 src.link(vqueue)
 
-aqueue.link(ars)
+aqueue.link(aconv)
+aconv.link(intersink)
+intersrc.link(ars)
 ars.link(aenc)
 aenc.link(flvmux)
 
